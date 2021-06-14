1f88cc6dcfbff92137cb364718fde66b1a8f5f75
GinleyP87 <83917798+GinleyP87@users.noreply.github.com>
2021-06-13 23:29:55 -0400

    final push

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,53 +1,97 @@
-// Get references to the #generate element
+// get reference to the #generate element
+
 var generateBtn = document.querySelector("#generate");
 
-// ARRAYS
+// ARRAYS 
+
 var num = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-var locase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-var upcase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-var specChar = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '=', '+', '|', ';', ':', '<', '>', ',', '.', '?', '/', '~', '~'
+var loCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var upCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+var specChar = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '=', '+', ';', ':', '<', '>', ',', '.', '?', '/', '~',
 ];
 
+// QUESTIONS
 
 function questions() {
-  var process = false;
-    do {
-      var charL = prompt("Choose a password length between 8 and 128 characters.");
-      var askNum = confirm("Do you want your password to include numbers?");
-      var askLo = confirm("Do you want your password to include lower case letters?");
-      var askUp = confirm("Do you want your password to include upper case letters?");
-      var askSpec = confirm("Do you want your password to include special characters?");
+  var isValid = false;
+  do {
+    var charLength = prompt("Choose password length between 8 and 128 characters");
+    var askNum = confirm("Do you want your password to include numbers?");
+    var askLoCase = confirm("Do you want your password to include lower case letters?");
+    var askUpCase = confirm("Do you want your password to include upper case letters?");
+    var askSpecChar = confirm("Do you want your password to include special characters?");
+    
+    var responses = {
+      length: charLength,
+      askNum: askNum,
+      askLoCase: askLoCase,
+      askUpCase: askUpCase,
+      askSpecChar: askSpecChar
+    } 
+    
+    if((charLength < 8)||(charLength > 128))
+    alert("Choose number between 8 and 128");
+    else if((!askNum)&&(!askLoCase)&&(!askUpCase)&&(!askSpecChar))
+    alert("Must choose at least one type.");
+    else
+    isValid = true;
 
-      var answers = {
-          charL: charL,
-          askNum: askNum,
-          askLo: askLo,
-          askUp: askUp,
-          askSpec: askSpec
-      }
-      if((length < 8)||(length >128))
-      alert("Choose number between 8 and 128.")
-      else if ((!askNum)&(askLo)&&(askUp)&&(askSpec))
-      alert("Must choose at least one parameter.");
-      else 
-      process = true;
+  } while(!isValid);
   
-} while(!process);
-return answers;
+  return responses;
+
 }
 
+//CREATE RESULT
+
+function generatePassword() {
+  var possChar = questions();
+  var randoNum = [];
+  var pwFinal = "";
+
+  if (possChar.askNum) {
+    for (var i of num)
+      randoNum.push(i);
+  }
+
+  if (possChar.askLoCase) {
+    for (var i of loCase)
+      randoNum.push(i);
+  }
+
+  if (possChar.askUpCase) {
+    for (var i of upCase)
+      randoNum.push(i);
+  }
+
+  if (possChar.askSpecChar) {
+    for (var i of specChar)
+      randoNum.push(i);
+  }
 
 
+  console.log(randoNum);
 
+
+  for (var i = 0; i < possChar.length; i++) {
+    pwFinal += randoNum[Math.floor(Math.random() * randoNum.length)];
+    
+  }
+
+  console.log(pwFinal);
+
+  return pwFinal;
+
+}
 
 // Write password to the #password input
+
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
-
 // Add event listener to generate button
+
 generateBtn.addEventListener("click", writePassword);
